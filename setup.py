9e8d1cc320c259a284f9bffcb68ca4c74cb72ed2
Edoardo Grassi <edo.g@hotmail.it>
2019-10-17 12:06:51 +0200

    Fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='djsuperadmin',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/lotrekagency/djsuperadmin',
     install_requires=[
         'django-hvad @ https://github.com/lotrekagency/django-hvad/archive/master.zip',

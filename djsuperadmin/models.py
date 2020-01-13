d2843dda1edb7e3c866a6f7ebbbd35848c2e5f31
Andrea Stagi <stagi.andrea@gmail.com>
2020-01-13 18:18:09 +0100

    Identifier must be unique

--- djsuperadmin/models.py
+++ djsuperadmin/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class BaseContent(TranslatableModel):
-    identifier = models.CharField(max_length=200)
+    identifier = models.CharField(max_length=200, unique=True)
     translations = TranslatedFields(
         permalink=models.CharField(max_length=200, blank=True, null=True),
         content=models.TextField())

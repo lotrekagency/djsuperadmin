a8c33a7fea64ab7ee0937f36352111feac88ae21
bnznamco <gabriele.baldi.01@gmail.com>
2021-10-26 17:29:17 +0200

    chore: Use semantic versioning to publish to pypi

--- publish.sh
+++ publish.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-rm -rf dist build *.egg-info
-pip install twine
-python setup.py sdist bdist_wheel
-sudo python -m twine upload dist/*
+pip install python-semantic-release
+python-semantic-release publish
--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
 from setuptools import setup
+from djsuperadmin import __version__
 
-
-setup(
-    name='djsuperadmin',
-    version='0.9.0',
-    url='https://github.com/lotrekagency/djsuperadmin',
-    install_requires=[],
-    dependency_links=[],
-    long_description=open("README.rst", "r").read(),
-    description="Edit contents directly on your page with Django",
-    license="MIT",
-    author="Lotrèk",
-    author_email="dimmitutto@lotrek.it",
-    packages=['djsuperadmin', 'djsuperadmin.templatetags'],
-    include_package_data=True,
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3'
-    ]
-)
+setup(version=__version__)

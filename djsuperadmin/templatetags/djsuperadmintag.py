d5e239aa99e17ab36844a945042fb5bc567e17ee
bnznamco <gabriele.baldi.01@gmail.com>
2022-03-17 13:35:23 +0100

    fix: fix script injection

--- djsuperadmin/templatetags/djsuperadmintag.py
+++ djsuperadmin/templatetags/djsuperadmintag.py
@@ -5,10 +5,10 @@ from django.utils.safestring import mark_safe
 
 def _get_obj_span(obj, attribute, placeholder, editor_mode):
     span = '<span class="djsuperadmin"'
-    span += ' data-djsa-mode="%s" ' % editor_mode
-    span += ' data-djsa-id ="%s" ' % str(obj.id)
-    span += ' data-djsa-getcontenturl="%s" ' % str(obj.superadmin_get_url)
-    span += ' data-djsa-patchcontenturl="%s" ' % str(obj.superadmin_patch_url)
+    span += ' data-djsa-mode="%s"' % editor_mode
+    span += ' data-djsa-id="%s"' % str(obj.id)
+    span += ' data-djsa-getcontenturl="%s"' % str(obj.superadmin_get_url)
+    span += ' data-djsa-patchcontenturl="%s"' % str(obj.superadmin_patch_url)
     span += ">%s</span>" % getattr(obj, attribute, placeholder)
     return span
 
@@ -44,7 +44,7 @@ def djsuperadminjs(context):
             os.path.join(superadmin_basedir, "..", "dist", "djsuperadmin.bundle.js"),
             "r",
         ) as js_file:
-            js = 'script src="https://cdn.ckeditor.com/4.12.1/standard/ckeditor.js"></script>'
+            js = '<script src="https://cdn.ckeditor.com/4.12.1/standard/ckeditor.js"></script>'
             js += '<script>var djsa_logout_url="%s";%s</script>' % ("", js_file.read())
         return mark_safe(js)
     return ""
